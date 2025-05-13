c470921a827fde33bc7f2dfcac541f5d4fb3801a
Aezan <110289332+AezanPathan@users.noreply.github.com>
2025-05-13 12:22:49 +0530

    [dictionary decoder]

--- src/Decoder.cs
+++ src/Decoder.cs
@@ -8,7 +8,7 @@ public class Decoder
         if (string.IsNullOrEmpty(input))
             throw new ArgumentException("Nothing to decode", nameof(input));
 
-        if (Char.IsDigit(input[0])) //if string 
+        if (Char.IsDigit(input[0]))
             return DecodeString(input);
 
         else if (input[0] == 'i')
@@ -16,17 +16,12 @@ public class Decoder
 
         else if (input[0] == 'l')
             return DecodeList(input);
+
+        else if (input[0] == 'd')
+            return DecodeDictionary(input);
+
         else
             throw new InvalidOperationException($"Unknown bencode type '{input[0]}'");
-
-        // return input[0] switch
-        // {
-
-        //     var c when char.IsDigit(c) => DecodeString(input),
-        //     'i' => DecodeInteger(input),
-        //     'l' => DecodeList(input),
-        //     _ => throw new InvalidOperationException($"Unknown bencode type '{input[0]}'")
-        // };
     }
 
     private (string, int) DecodeString(string stringToDecode)
@@ -53,15 +48,29 @@ public class Decoder
 
         // consumed length is up through and including that 'e'
         return (long.Parse(numStr), end + 1);
+    }
 
-        // Console.WriteLine(stringToDecode.Substring(1, stringToDecode.Length - 2)
+    private (Dictionary<string, object>, int) DecodeDictionary(string stringToDecode)
+    {
+        var dict = new Dictionary<string, object>();
+        var offset = 1; // skip 'd'
 
-        // “i<digits>e”
-        // int end = stringToDecode.IndexOf('e');
-        // if (end < 0) throw new FormatException("Missing 'e' in integer token");
+        while (offset < stringToDecode.Length && stringToDecode[offset] != 'e')
+        {
+            // Decode key (must be string)
+            var (key, keyUsed) = DecodeString(stringToDecode.Substring(offset));
+            offset += keyUsed;
 
-        // string num = stringToDecode.Substring(1, stringToDecode.Length - 2);
-        // return (long.Parse(num), stringToDecode.Length + 1);
+            // Decode value
+            var (value, valueUsed) = DecodeInput(stringToDecode.Substring(offset));
+            dict.Add(key, value);
+            offset += valueUsed;
+        }
+
+        if (offset >= stringToDecode.Length || stringToDecode[offset] != 'e')
+            throw new FormatException("Unterminated dictionary");
+
+        return (dict, offset + 1);
     }
 
     private (List<object>, int) DecodeList(string s)
@@ -82,93 +91,4 @@ public class Decoder
 
         return (list, offset + 1);
     }
-
-    ///public enum DecodeTypes { Unknown, String, Integer, List }
-
-    // private (string, int) DecodeString(string stringToDecode)
-    // {
-    //     int colonIndex = stringToDecode.IndexOf(':');
-    //     if (colonIndex != -1)
-    //     {
-    //         int strLength = int.Parse(stringToDecode[..colonIndex]);
-    //         string strValue = stringToDecode.Substring(colonIndex + 1, strLength);
-    //         return (strValue, strLength + 1 + colonIndex);
-    //     }
-    //     else
-    //     {
-    //         throw new InvalidOperationException("Invalid encoded value: " +
-    //                                             stringToDecode);
-    //     }
-    // }
-    // private (long, int) DecodeInteger(string stringToDecode)
-    // {
-    //     int colonIndex = stringToDecode.IndexOf('e');
-    //     if (colonIndex != -1)
-    //     {
-    //         string strValue = stringToDecode.Substring(1, colonIndex - 1);
-    //         return (long.Parse(strValue), strValue.Length + 2);
-    //     }
-    //     else
-    //     {
-    //         throw new InvalidOperationException("Invalid encoded value: " +
-    //                                             stringToDecode);
-    //     }
-    // }
-    // private (List<object>, int) DecodeList(string stringToDecode)
-    // {
-    //     List<object> result = [];
-    //     int offset = 1;
-    //     while (offset < stringToDecode.Length)
-    //     {
-    //         if (stringToDecode[offset] == 'e')
-    //             break;
-    //         (object element, int elementOffset) =
-    //             DecodeInput(stringToDecode[offset..]);
-    //         result.Add(element);
-    //         offset += elementOffset;
-    //     }
-    //     return (result, offset + 1);
-    // }
-
-    // public DecodeTypes GetDecodeType(char firstSymbol)
-    // {
-    //     return firstSymbol switch
-    //     {
-    //         _ when char.IsDigit(firstSymbol) => DecodeTypes.String,
-    //         'i' => DecodeTypes.Integer,
-    //         'l' => DecodeTypes.List,
-    //         _ => DecodeTypes.Unknown,
-    //     };
-    // }
-    // public (object, int) DecodeInput(string stringToDecode)
-    // {
-    //     return GetDecodeType(stringToDecode[0]) switch
-    //     {
-    //         DecodeTypes.String => DecodeString(stringToDecode),
-    //         DecodeTypes.Integer => DecodeInteger(stringToDecode),
-    //         DecodeTypes.List => DecodeList(stringToDecode),
-    //         _ or DecodeTypes.Unknown => throw new InvalidOperationException(
-    //             "Unhandled encoded value: " + stringToDecode),
-    //     };
-    // }
-
-    /*
-    private (List<object>, int) DecodeList(string stringToDecode)
-        {
-            List<object> result = [];
-            int offset = 1;
-            while (offset < stringToDecode.Length)
-            {
-                if (stringToDecode[offset] == 'e')
-                    break;
-                (object element, int elementOffset) =
-                    DecodeInput(stringToDecode[offset..]);
-                result.Add(element);
-                offset += elementOffset;
-            }
-            return (result, offset + 1);
-        }
-
-    */
 }
-
