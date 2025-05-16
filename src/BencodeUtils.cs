7261687de468d8c6db469f0465c8ff6799589a69
Aezan <110289332+AezanPathan@users.noreply.github.com>
2025-05-16 20:26:44 +0530

    [info dict encode]

--- src/BencodeUtils.cs
+++ src/BencodeUtils.cs
@@ -7,31 +7,51 @@ public class BencodeUtils
     public static int FindMarkerPosition(byte[] data, string marker)
     {
         byte[] markerBytes = Encoding.ASCII.GetBytes(marker);
-        for (int i = 0; i < data.Length - markerBytes.Length; i++)
+        for (int i = 0; i <= data.Length - markerBytes.Length; i++)
         {
-            if (data[i..(i + markerBytes.Length)].SequenceEqual(markerBytes)) return i;
-
+            if (data[i..(i + markerBytes.Length)].SequenceEqual(markerBytes))
+                return i;
         }
         throw new KeyNotFoundException("Marker not found");
     }
 
-    public static int FindMatchingEnd(byte[] data, int start)
+    public static int FindMatchingEnd(byte[] data, int startIndex)
     {
         int depth = 1;
-        for (int i = start + 1; i < data.Length; i++)
+        int i = startIndex + 1; // Start after 'd'
+
+        while (i < data.Length && depth > 0)
         {
-            switch ((char)data[i])
+            byte current = data[i];
+            if (current == 'd' || current == 'l')
             {
-                case 'd':
-                case 'l':
-                    depth++;
-                    break;
-                case 'e':
-                    depth--;
-                    if (depth == 0) return i;
-                    break;
+                depth++;
+                i++;
+            }
+            else if (current == 'e')
+            {
+                depth--;
+                i++;
+                if (depth == 0)
+                    return i - 1; // Position of closing 'e'
+            }
+            else if (char.IsDigit((char)current))
+            {
+                // Skip over string (e.g., "3:foo")
+                int colon = Array.IndexOf(data, (byte)':', i);
+                if (colon == -1) break;
+                int length = int.Parse(Encoding.ASCII.GetString(data, i, colon - i));
+                i = colon + 1 + length; // Move past the string
+            }
+            else
+            {
+                i++;
             }
         }
-        throw new FormatException("Unterminated dictionary");
+
+        if (depth != 0)
+            throw new FormatException("Unclosed structure");
+
+        return i - 1;
     }
 }
